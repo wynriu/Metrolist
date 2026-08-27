b3ddbdd405f2d8dfdf33f6e61dda63799f148e82
Vin <wyng1trial@gmail.com>
2026-08-27 12:47:13 +0700

    Make Canvas search previews square

--- app/src/main/kotlin/com/metrolist/music/features/canvas/ui/CanvasSearchDialog.kt
+++ app/src/main/kotlin/com/metrolist/music/features/canvas/ui/CanvasSearchDialog.kt
@@ -4,6 +4,7 @@ import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.animation.fadeIn
 import androidx.compose.animation.fadeOut
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -233,7 +234,7 @@ private fun CanvasSearchResultCard(
             Box(
                 modifier = Modifier
                     .fillMaxWidth()
-                    .height(170.dp)
+                    .aspectRatio(1f)
                     .clip(MaterialTheme.shapes.large),
             ) {
                 CanvasArtworkPlayer(
